6942b60a0e2be939c741848b7623e74e4f8fb3a3
Felipe Martinez <pipernene@gmail.com>
2021-08-26 17:09:46 +0200

    Added redis pubsub broker

---
+++ broker/redis/go.mod
@@ -0,0 +1,9 @@
+module github.com/MouseHatGames/mice-plugins/broker/redis
+
+go 1.15
+
+require (
+	github.com/MouseHatGames/mice v1.2.8
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-redis/redis/v8 v8.11.3 // indirect
+)
---
+++ broker/redis/redis.go
@@ -0,0 +1,62 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/MouseHatGames/mice/broker"
+	"github.com/MouseHatGames/mice/logger"
+	"github.com/MouseHatGames/mice/options"
+	"github.com/go-redis/redis/v8"
+)
+
+type redisBroker struct {
+	rdb *redis.Client
+	log logger.Logger
+}
+
+func Broker(ropt *redis.Options) options.Option {
+	return func(o *options.Options) {
+		o.Broker = &redisBroker{
+			rdb: redis.NewClient(ropt),
+			log: o.Logger.GetLogger("redispb"),
+		}
+	}
+}
+
+func (r *redisBroker) Close() error {
+	return r.rdb.Close()
+}
+
+func (r *redisBroker) Publish(ctx context.Context, topic string, data *broker.Message) error {
+	cmd := r.rdb.Publish(ctx, topic, data.Data)
+	if cmd.Err() != nil {
+		return cmd.Err()
+	}
+
+	r.log.Debugf("published message %s to %d receivers", topic, cmd.Val())
+	return nil
+}
+
+func (r *redisBroker) Subscribe(ctx context.Context, topic string, callback func(*broker.Message)) error {
+	pubsub := r.rdb.Subscribe(ctx, topic)
+
+	rectx, c := context.WithTimeout(context.Background(), 10*time.Second)
+	defer c()
+
+	_, err := pubsub.Receive(rectx)
+	if err != nil {
+		return fmt.Errorf("subscribe to topic: %w", err)
+	}
+
+	go func() {
+		c := pubsub.Channel()
+
+		for msg := range c {
+			callback(&broker.Message{Data: []byte(msg.Payload)})
+		}
+	}()
+
+	return nil
+}
